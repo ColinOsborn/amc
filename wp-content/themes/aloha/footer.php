d3e1c07adbb837669779ed50619babdef6484ea5
Colin Osborn <colin@Colins-MacBook-Pro.local>
2018-09-05 22:31:10 -0600

    added assets for team plugin

--- wp-content/themes/aloha/footer.php
+++ wp-content/themes/aloha/footer.php
@@ -4,7 +4,7 @@
                 <div class="copyright col-sm-7 col-4">
                     <p>Copyright ColinOsborn Designs 2018</p>
                 </div>
-                <nav class="footer-menu col-sm-5 col-8 text-right">
+                <nav class="footer-menu col-sm-12 col-4 text-right">
                     <?php wp_nav_menu( array('theme_location' => 'footer_menu')); ?>
                 </nav>
             </div>
