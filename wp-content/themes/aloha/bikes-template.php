bfc7acbb8be1b8f44b1301182d402d4889eff4a4
Colin Osborn <colin@Colins-MacBook-Pro.local>
2018-10-08 22:50:57 -0600

    more logos and formatting

--- wp-content/themes/aloha/bikes-template.php
+++ wp-content/themes/aloha/bikes-template.php
@@ -51,21 +51,22 @@ Template Name: Bike Template
                                 <p>Moots. They're made in Steamboat Springs out of sweet sweet Ti-Tanium. What else do we really need to say. Crack a beer folks. Kokanee you mother fuckers drink it up!! I'm putting space in here so shit looks right? eh?! </p>
                             </div>
                         </div>
-                        <br>
+                    </div>
+                    <br>
                     <div class="container-middle">
-                    <div class="one-of-three">
-                    <div class="bike-lines"><a href="http://www.devinci.com/" target="_blank"><img src="http://dirtyfingersbikes.com/wp-content/uploads/2016/08/devinci.png"></a></div>
-                    <p>All the way from the French-speaking province of Quebec, Canada, Devinci is making bikes that blow our mind. Offering everything from full-on downhill bikes to enduro-style bikes to road bikes, Devinci is at the forefront of the ever-growing carbon bike explosion. Ride one. You’ll want one.</p>
-                    </div>
-                    <div class="two-of-three">
-                    <div class="bike-lines"><a href="http://www.konaworld.com/" target="_blank"><img src="http://dirtyfingersbikes.com/wp-content/uploads/2016/08/kona_logo.png"></a></div>
-                    <p>Our favorite all-around bike company, Kona makes a bike for any use or user. Mountain, road, cross, commute, or kids… Every single Kona we throw a leg over fits great and feels ready to shred, right out of the box. Located in the Northwest and making bikes since 1988, Kona offers selection, durability, and bikes for any budget.</p>
-                    </div>
-                    <div class="three-of-three">
-                    <div class="bike-lines"><a href="https://www.transitionbikes.com/" target="_blank"><img src="http://dirtyfingersbikes.com/wp-content/uploads/2016/08/transition.png"></a></div>
-                    <p>Transition is another Northwest company, and it’s staffed by riders from the ground floor up. These folks are dedicated to making fun-and-fast downhill, trail, and dirt jump bikes. They offer bikes in a variety of travels and wheel sizes with one thing in common: great handling at speed. If you need a one-bike quiver that can handle anything – 44 Road cross-country, Post Canyon jumps and Syncline rocks – Transition is for you.</p>
-                    </div>
-                    </div>
+                        <div class="one-of-three">
+                            <div class="bike-lines"><a href="http://www.salsacycles.com/" target="_blank"><img src="http://localhost:8888/aloha/wp-content/uploads/2018/10/salsa-logo-red-150.png"></a></div>
+                                <p>Salsa Baby! Who does'nt like salsa? Offering everything from full-on downhill bikes to enduro-style bikes to road bikes, Devinci is at the forefront of the ever-growing carbon bike explosion. Ride one. You’ll want one.</p>
+                            </div>
+                        <div class="two-of-three">
+                            <div class="bike-lines"><a href="https://surlybikes.com/" target="_blank"><img src="http://localhost:8888/aloha/wp-content/uploads/2018/10/surly-bikes-logo-150.png"></a></div>
+                                <p>Surly! Whose surly?! I'm fucking Surly and you know you are too! Come ride this Pugsley. Feels like hard steel.</p>
+                            </div>
+                        <div class="three-of-three">
+                            <div class="bike-lines"><a href="https://www.whycycles.com/" target="_blank"><img src="http://dirtyfingersbikes.com/wp-content/uploads/2016/08/transition.png"></a></div>
+                                <p>Why Cycles staffed by riders from the ground floor up. These folks are dedicated to making fun-and-fast downhill, trail, and dirt jump bikes. They offer bikes in a variety of travels and wheel sizes with one thing in common: great handling at speed. If you need a one-bike quiver that can handle anything – 44 Road cross-country, Post Canyon jumps and Syncline rocks – Transition is for you.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -74,4 +75,5 @@ Template Name: Bike Template
 
     </main>
     </div>
+
 <?php get_footer(); ?>
