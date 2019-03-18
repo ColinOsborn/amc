0b632449fb606e2f6f6a2f51465344cd272c37d8
Colin Osborn <colin@Colins-MacBook-Pro.local>
2019-03-17 23:00:55 -0600

    last tweaks and more photo updates

--- wp-content/themes/aloha/footer.php
+++ wp-content/themes/aloha/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="copyright col-sm-7 col-4">
-                    <p>Copyright ColinOsborn Designs 2018</p>
+                    <p><sup>Copyright ColinOsborn Designs 2018</sup></p>
                 </div>
                 <nav class="footer-menu col-sm-12 col-4 text-right">
                     <?php wp_nav_menu( array('theme_location' => 'footer_menu')); ?>
